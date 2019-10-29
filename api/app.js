f5bcfc85409345fe30a089ffc24a4663b09e678c
alex-kostenko <0984181760ss@gmail.com>
2019-10-29 17:02:09 +0200

    temp

--- api/app.js
+++ api/app.js
@@ -1,48 +1,130 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var cors = require("cors");
+// var createError = require("http-errors");
+// var express = require("express");
+// var path = require("path");
+// var cookieParser = require("cookie-parser");
+// var logger = require("morgan");
+// var cors = require("cors");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var testAPIRouter = require("./routes/testAPI");
-var testDBRouter = require("./routes/testDB");
+// var indexRouter = require("./routes/index");
+// var usersRouter = require("./routes/users");
+// var testAPIRouter = require("./routes/testAPI");
+// var testDBRouter = require("./routes/testDB");
+// var userLogin = require("./routes/login");
 
-var app = express();
+// var app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+// // view engine setup
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "jade");
 
-app.use(cors());
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(cors());
+// app.use(logger("dev"));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, "public")));
+
+// app.use("/", indexRouter);
+// app.use("/users", usersRouter);
+// app.use("/testAPI", testAPIRouter);
+// app.use("/testDB", testDBRouter);
+// app.use("/login", userLogin);
+
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render("error");
+// });
+
+// const http = require("http");
+
+// let message = "UPD";
+// http.createServer(function (request, response) {
+
+//     response.end(message);
+
+// }).listen(3000, "127.0.0.1", () => {
+//     console.log("----------------------------------------------------------------".toLocaleUpperCase());
+//     console.log("---------------Сервер начал прослушивание запросов--------------".toLocaleUpperCase());
+//     console.log("----------------------------------------------------------------".toLocaleUpperCase());
+// });
+
+// module.exports = app;
+
+const express = require("express");
+const app = express();
+const users = require("./routes/users");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const passport = require("passport");
+
+const User = require("./controllers/users");
+
+const path = __dirname + "/views/";
+const port = process.env.PORT || 9000;
+const LocalStrategy = require("passport-local").Strategy;
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(
+  new LocalStrategy(function (username, password, done) {
+    User.getUserByUsername(username, function (err, user) {
+      if (err) throw err;
+      if (!user) {
+        return done(null, false, { message: "Unknown User" });
+      }
+      User.comparePassword(password, user.password, function (err, isMatch) {
+        if (err) console.log("ERROR: \n", err);
+        if (isMatch) {
+          return done(null, user);
+        } else {
+          return done(null, false, { message: "Invalid password" });
+        }
+      });
+    });
+  })
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.getUserById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+app.set("view engine", "html");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path));
+app.use("/auth", users);
+
+// BodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/testAPI", testAPIRouter);
-app.use("/testDB", testDBRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render("error");
-});
+// Express Session
+app.use(
+  session({
+    secret: "secret",
+    saveUninitialized: true,
+    resave: true
+  })
+);
 
 const http = require("http");
 
@@ -56,5 +138,3 @@ http.createServer(function (request, response) {
     console.log("---------------Сервер начал прослушивание запросов--------------".toLocaleUpperCase());
     console.log("----------------------------------------------------------------".toLocaleUpperCase());
 });
-
-module.exports = app;
--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect} from 'react';
+import HomePage from './pages/home/index.js';
 import { Button, Radio, Icon } from 'antd';
 
 const App = () => {
 
   const [apiResponse, setApiResponse] = useState(0);
   const [dbResponse, setDbResponse] = useState(0);
+  const [login, setLogin] = useState(0);
 
   useEffect(() => {
     fetch("http://localhost:9000/testAPI")
@@ -16,16 +18,23 @@ const App = () => {
       .then(res => res.text())
       .then(res => setDbResponse(res))
       .catch(err => err);
+
+    fetch("http://localhost:9000/login")
+      .then(res => res.text())
+      .then(res => setLogin(res))
+      .catch(err => err);
   }, [])
-  console.log(React);
+
   return(
     <div className="App">
       <div>{apiResponse}</div>
       <div>{dbResponse}</div> 
+      <div>{login}</div>
       <Button type="primary">Button</Button>
       <Radio type="primary" checked={true}/>
       <Icon type="step-backward" theme="filled" />
       <Icon type="down-square" theme="filled" />
+      <HomePage/>
     </div>
   );
 };
